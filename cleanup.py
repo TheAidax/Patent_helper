49c2a3d038fd372ea02165dd0756b9c22c055d08
Rayhan A <99364406+Andaray@users.noreply.github.com>
2023-04-03 17:31:12 -0400

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -36,14 +36,48 @@ def cleanTxt(filePath):
 					if flag ==1:
 						print("Starting to write")
 						newFile.write(row)
-				
-				
-		
-					
-
 	file.close()
 	fp.close()
 	newFile.close()
+##################################################################################
+		# opening and creating new .txt file
+	filePath = 'upToDescription.txt'
+	with open(
+		filePath, 'r') as r, open(
+			'stripped.txt', 'w') as o:
+	
+		for line in r:
+			#strip() function
+			if line.strip():
+				o.write(line)
+	
+	r.close()
+	o.close()
+##################################################################################
+	filePath = "stripped.txt"
+	newFile = open("2chars.txt",'w')	
+	with open(filePath, 'r+') as fp:
+		
+		lines = fp.readlines()
+		for row in lines:
+			# check if string present on a current line
+			#print(row.find(word))
+			# find() method returns -1 if the value is not found,
+			# if found it returns index of the first occurrence of the substring
+			if len(row) > 3:
+				print("len of line > 3: ",len(row))
+				newFile.write(row)
+
+			
+			
+	fp.close()
+	newFile.close()
+	
 
 
-cleanTxt("parent.txt")
+
+
+
+
+
+cleanTxt("CIP.txt")
