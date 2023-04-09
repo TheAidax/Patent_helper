ef9a69755ef280ee2de5d9853e8e4ea04f74580e
TheAidax <97730723+TheAidax@users.noreply.github.com>
2023-04-09 14:54:50 -0400

    Add files via upload
    Changes made to compare.py

--- Patent_compare_v2/compare.py
+++ Patent_compare_v2/compare.py
@@ -1,21 +1,62 @@
+from pdfminer.high_level import extract_text, extract_pages
+from difflib import SequenceMatcher # from difflib module for difflib plagarism check
+
+
+
+
+
+#############################################################################################################
+#                          Find differences in documents and display on front end                           #
+#                                                                                                           #
+#############################################################################################################
+
+
+
+
 def compare():
-    # Run find changes and plagarism check 
-    x = 0
+    # Master prog: Run find changes and plagarism check functions
+    resultChanges = findChanges()
+    resultPlagarism = plagarismCheck()
+    result = determine(resultPlagarism, resultChanges)
+    display(result)
+
+
+
+
+
+###################################################################################################################### END compare()
+
+
+
+
 
 
 def findChanges():
-    # find basic additions and removals from the text file
+    # find basic additions and removals from the text file using difflib
     x = 0
+
+
+
+
+###################################################################################################################### END findchanges()
 
 
 
 def plagarismCheck():
-    # Check the text file for plagarism
+    # Check the text file for plagarism by vectorizing text and looking for sin-cos diffference?? 
+    x = 0
+ 
+
+
+
+ ###################################################################################################################### END plagarismCheck()
+ 
+ 
+def importedCheck():
+    #  Imported method for checking for plagarism using difflib
     x = 0
 
-    # Importing SequenceMatcher
-# from difflib module
-from difflib import SequenceMatcher
+
 
 # Declaring string variables
 string1 = 'I am geek' #Should be from parent
@@ -29,11 +70,79 @@ match = SequenceMatcher(None,
 # and multiplying it with 100
 result = match.ratio() * 100
 
-# Display the final result
+
+#Display the final result
 print(int(result), "%")
 
 
+###################################################################################################################### END importedCheck()
 
-def display(result_doc, result):
+
+
+
+def determine():
+    # Determine the percent difference between docs                                  
+    # Should return a percent based on all tests.      
+
+    x = 0
+
+
+
+
+
+
+
+
+
+###################################################################################################################### END determine()
+
+
+
+
+
+
+
+
+
+
+#############################################################################################################
+#                          Display the differences visually in the front end                                #
+#                                                                                                           #
+#############################################################################################################
+
+def display(result):
     # Take the output of the plagarism checker and change finder and display 
     x = 0
+
+
+
+###################################################################################################################### END display(result_doc, result)
+
+
+
+
+
+
+
+#############################################################################################################
+#                          Import files for comparison                                                      #
+#                            (Imported code)                                                                #
+#############################################################################################################
+
+
+
+
+def pulling_Text(parentFilepath,CIPFilepath):
+    getParentText = extract_text(parentFilepath)
+    getCIPText = extract_text(CIPFilepath)
+
+    parentText = open("parent.txt","w")
+    CIPText = open("CIP.txt","w")
+
+    parentText.write(getParentText)
+    CIPText.write(getCIPText)
+
+    parentText.close()
+    CIPText.close()
+
+pulling_Text("parentMadeSearchable.pdf","child.pdf")
