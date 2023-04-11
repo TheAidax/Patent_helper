a3910e3117f25e7a5803e16669295902165331cf
renancharles-pierre <RCharlesPierre1@pride.hofstra.edu>
2023-04-10 22:44:59 -0400

    some javascript additions

--- US_Patent_Helper/Controllers/HomeController.cs
+++ US_Patent_Helper/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using US_Patent_Helper.Models;
+using System.Linq;
+using System.Web;
+
 
 namespace US_Patent_Helper.Controllers;
 
@@ -15,6 +18,30 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+       /* String path = Server.MapPath("~/images/");
+        String[] imagesfiles = Directory.GetFiles(path);
+        ViewBag.images = imagesfiles; 
+
+             @foreach(var file in ViewBag.images)
+{ 
+                <img src ="~/images/@Path.GetFileName(file)"/>
+} */
+
+        /* 
+         * Giannis
+         * Middleton
+         * Lopez
+         * Matthews
+         * Holiday
+         * Thanansis
+         * Connington
+         * Portis
+         * WHite boy from Utah
+         * Jevon Carter
+         * Shooter w dreads that played for the CEltics a few years ago
+         * Greyson Allen
+         */
+
         return View();
     }
 
