a9c321edfbef6ffe9e84eaa3111025c93db9845c
Rayhan A <99364406+Andaray@users.noreply.github.com>
2023-04-25 16:52:10 -0400

    Update Program.cs

--- RunPythonThruThis/Program.cs
+++ RunPythonThruThis/Program.cs
@@ -8,7 +8,7 @@ namespace RunPythonFromCS {
         static void Main(string[] args)
         {
             Console.WriteLine("Execute python process...");
-            ExecProcess2();
+            createTuples();
         }
 
         /*
