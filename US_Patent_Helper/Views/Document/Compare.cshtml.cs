6130452e80e7f52a5d72b8fef4a76cfe297e07bf
renancharles-pierre <RCharlesPierre1@pride.hofstra.edu>
2023-05-09 17:15:01 -0400

    some code tweaks

--- US_Patent_Helper/Views/Document/Compare.cshtml.cs
+++ US_Patent_Helper/Views/Document/Compare.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+    
 
 namespace US_Patent_Helper.Views.Document
 {
@@ -13,10 +14,14 @@ namespace US_Patent_Helper.Views.Document
         {
         }
 
-        public void OnClick()
+
+      //  [WebMethod]
+        public static void MyFunction()
         {
-            Console.WriteLine("Button Clicked");
-     
+            
         }
+
     }
+
 }
+
